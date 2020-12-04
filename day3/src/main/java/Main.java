ea1f3aeaa8f98dae2465eb40edabfe4920849870
Larqqa <32098438+Larqqa@users.noreply.github.com>
2020-12-04 07:26:41 +0200

    feature: added part 2 to day 3

--- day3/src/main/java/Main.java
+++ day3/src/main/java/Main.java
@@ -1,35 +1,48 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream inputStream = classloader.getResourceAsStream("input.txt");
-        InputStreamReader isReader = new InputStreamReader(inputStream);
-        BufferedReader reader = new BufferedReader(isReader);
-        StringBuffer stringBuffer = new StringBuffer();
+    private static Integer width = 0;
+    private static String str;
+    private static Long multiplicant;
 
-        // get width from first line
-        Integer width = 0;
-        String str;
+    static ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    static InputStream inputStream = classloader.getResourceAsStream("input.txt");
+    static InputStreamReader isReader = new InputStreamReader(inputStream);
+    static BufferedReader reader = new BufferedReader(isReader);
+    static StringBuffer stringBuffer = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
         while((str = reader.readLine())!= null){
             width = str.length();
             stringBuffer.append(str);
         }
-        //System.out.println(stringBuffer.toString());
-        //System.out.println(width);
 
         String[] map;
         map = stringBuffer.toString().split("");
 
+        multiplicant = count_trees(map, 1, 1);
+        multiplicant *= count_trees(map, 3, 1);
+        multiplicant *= count_trees(map, 5, 1);
+        multiplicant *= count_trees(map, 7, 1);
+        multiplicant *= count_trees(map, 1, 2);
+        System.out.println(multiplicant);
+    }
+
+    private static Long count_trees(String[] map, Integer offsetX, Integer offsetY) {
         //System.out.println(map[6 + width * 2]);
+
         Integer dots = 0;
         Integer trees = 0;
         Integer x = 0;
-        for (Integer y = 1; map.length / width > y; y += 1) {
 
-            x += 3;
-            if (x >= width) x = 0 + (x - width);
+        for (Integer y = 1; map.length / width > y; y += offsetY) {
+
+            x += offsetX;
+            if (x >= width) {
+                x = 0 + (x - width);
+            }
 
             if (map[x + width * y].equals(".")) {
                 dots++;
@@ -38,7 +51,7 @@ public class Main {
             }
         }
 
-        System.out.println(dots);
-        System.out.println(trees);
+        System.out.printf("%s, %s\n", dots, trees);
+        return Long.valueOf(trees);
     }
 }
